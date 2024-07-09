1a7605a78fdfce076e8154362e3fc2471b403e38
Lukidom <122173735+Lukidom@users.noreply.github.com>
2024-07-09 17:49:47 -0600

    commit 2

--- blog.js
+++ blog.js
@@ -1,28 +1,51 @@
 const articles = [
 	{
-		id: 1,
-		title: 'Septimus Heap Book One: Magyk',
-		date: 'July 5, 2022',
-		description:
-			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
-		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
-		imgAlt: 'Book cover for Septimus Heap 1',
-		ages: '10-14',
-		genre: 'Fantasy',
-		stars: '****'
+	  id: 1,
+	  title: 'Septimus Heap Book One: Magyk',
+	  date: 'July 5, 2022',
+	  description:
+		'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+	  imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
+	  imgAlt: 'Book cover for Septimus Heap 1',
+	  ages: '10-14',
+	  genre: 'Fantasy',
+	  stars: '****'
 	},
 	{
-		id: 2,
-		title: 'Magnus Chase Book One: Sword of Summer',
-		date: 'December 12, 2021',
-		description:
-			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
-		imgSrc:
-			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
-		imgAlt: 'Book cover for Magnus Chase 1',
-		ages: '12-16',
-		genre: 'Fantasy',
-		stars: '⭐⭐⭐⭐'
+	  id: 2,
+	  title: 'Magnus Chase Book One: Sword of Summer',
+	  date: 'December 12, 2021',
+	  description:
+		'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+	  imgSrc:
+		'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+	  imgAlt: 'Book cover for Magnus Chase 1',
+	  ages: '12-16',
+	  genre: 'Fantasy',
+	  stars: '⭐⭐⭐⭐'
 	}
-]
-// #FOBEGF tan #184543 green #83DOC6 light
+  ];
+  
+  const articleContainer = document.querySelector('.container');
+  
+  function buildArticles() {
+	articles.forEach((article) => {
+	  const newArticle = document.createElement('article');
+	  newArticle.className = 'article';
+  
+	  const articleHTML = `
+		<h2>${article.title}</h2>
+		<p>Published on ${article.date}</p>
+		<img src="${article.imgSrc}" alt="${article.imgAlt}">
+		<p>${article.description}</p>
+		<p>Ages: ${article.ages}</p>
+		<p>Genre: ${article.genre}</p>
+		<p>Rating: ${article.stars}</p>
+	  `;
+  
+	  newArticle.innerHTML = articleHTML;
+	  articleContainer.appendChild(newArticle);
+	});
+  }
+  
+  buildArticles();
